f5f7fa7454919fda6c8b07ba0e4eb1c73d0ff881
Hochan Kang <kanghc0217@naver.com>
2024-02-11 21:41:56 +0900

    feat: Recenter automatically

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,49 +1,48 @@
-import { memo, useEffect, useState } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { memo, useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
 import Place from 'types/Place';
 import styles from './Map.module.css';
 
 interface MapProps {
+  center: LatLngExpression;
   places: Place[];
   categoryFilter: string[];
 }
 
-function Map({ places, categoryFilter }: MapProps) {
-  const [center, setCenter] = useState<LatLngExpression | null>(null);
+function Map({ center, places, categoryFilter }: MapProps) {
+  function RecenterAutomatically({ center }: { center: LatLngExpression }) {
+    const map = useMap();
 
-  useEffect(() => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(position => {
-        setCenter([position.coords.latitude, position.coords.longitude]);
-      });
-    } else {
-      setCenter([37.5537586, 126.9809696]); // 서울의 중심 남산
-    }
-  }, []);
+    useEffect(() => {
+      map.setView(center);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [center]);
+
+    return null;
+  }
 
   return (
-    center && (
-      <MapContainer center={center} zoom={17} style={{ height: '100vh', width: '100vw' }}>
-        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-        {places
-          .filter(({ category, latitude, longitude }) => categoryFilter.includes(category) && latitude && longitude)
-          .map(({ id, category, name, menu, price, contact, address, latitude, longitude }) => (
-            <Marker key={id} position={[latitude as number, longitude as number]}>
-              <Popup>
-                <div className='default'>
-                  <h3 className={styles.title}>{`[${category}] ${name}`}</h3>
-                  <ul className={styles.list}>
-                    <li>{`${menu} ${price}원`}</li>
-                    <li>{address}</li>
-                    <li>{contact}</li>
-                  </ul>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-      </MapContainer>
-    )
+    <MapContainer center={center} zoom={17} style={{ height: '100vh', width: '100vw' }}>
+      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      {places
+        .filter(({ category }) => categoryFilter.includes(category))
+        .map(({ id, category, name, menu, price, contact, address, latitude, longitude }) => (
+          <Marker key={id} position={[latitude as number, longitude as number]}>
+            <Popup>
+              <div className='default'>
+                <h3 className={styles.title}>{`[${category}] ${name}`}</h3>
+                <ul className={styles.list}>
+                  <li>{`${menu} ${price}원`}</li>
+                  <li>{address}</li>
+                  <li>{contact}</li>
+                </ul>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      <RecenterAutomatically center={center} />
+    </MapContainer>
   );
 }
 
