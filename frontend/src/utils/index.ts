08a9f899c2e29bb70e92d5fbb399dc2a6f7b6b7b
Hochan Kang <kanghc0217@naver.com>
2024-02-12 11:20:17 +0900

    feat: Add warning message if can't find geolocation

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,9 +1,13 @@
+import { message } from 'antd';
 import { LatLngExpression } from 'leaflet';
 
 export const gotoUserLocation = (setPosition: (position: LatLngExpression) => void) => () => {
-  if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(userPosition => {
-      setPosition([userPosition.coords.latitude, userPosition.coords.longitude]);
-    });
+  if ('geolocation' in navigator && navigator.geolocation.getCurrentPosition) {
+    navigator.geolocation.getCurrentPosition(
+      userPosition => {
+        setPosition([userPosition.coords.latitude, userPosition.coords.longitude]);
+      },
+      () => message.warning('현 위치를 찾을 수 없습니다.', 1.5),
+    );
   }
 };
