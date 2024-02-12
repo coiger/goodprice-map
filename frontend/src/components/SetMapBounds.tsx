1d8b1fbc7174aaf431d32f4bbfed615e7851a632
Hochan Kang <kanghc0217@naver.com>
2024-02-12 11:19:53 +0900

    fix: Load event is not working

--- frontend/src/components/SetMapBounds.tsx
+++ frontend/src/components/SetMapBounds.tsx
@@ -1,16 +1,16 @@
 import { LatLngBounds } from 'leaflet';
 import { memo } from 'react';
-import { useMapEvents } from 'react-leaflet';
+import { useMap, useMapEvents } from 'react-leaflet';
 
 interface SetMapBoundsProps {
   setMapBounds: (mapBounds: LatLngBounds) => void;
 }
 
 function SetMapBounds({ setMapBounds }: SetMapBoundsProps) {
+  const map = useMap();
+  setMapBounds(map.getBounds());
+
   useMapEvents({
-    load: e => {
-      setMapBounds(e.target.getBounds());
-    },
     zoomend: e => {
       setMapBounds(e.target.getBounds());
     },
