b93bfa6095ce1fd14526aa1d289094484ac4e2d9
Hochan Kang <kanghc0217@naver.com>
2024-02-12 11:47:25 +0900

    feat: Use warning instead of error msg

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -22,7 +22,7 @@ function Search({ setPosition }: SearchProps) {
       if (status === kakao.maps.services.Status.OK && data[0].y && data[0].x) {
         setPosition([data[0].y, data[0].x]);
       } else {
-        message.error('검색된 장소가 없습니다.', 1);
+        message.warning('검색된 장소가 없습니다.', 1);
       }
     });
   };
